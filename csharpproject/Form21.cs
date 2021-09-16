c1ed1fe7591e28a553a4d18fac99717618486006
piyushjoshi11 <Piyushjoshi1101@gmail.com>
2021-09-16 19:22:45 +0530

    added new form , coded admin login form , added default values to admin table.

--- csharpproject/Form21.cs
+++ csharpproject/Form21.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using System.Data;
+
 
 
 namespace csharpproject
@@ -22,7 +22,27 @@ namespace csharpproject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\piyus\Source\Repos\attendance_mgt_system\csharpproject\Schooldb.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT *FROM Admin where id = @id and password= @password", con);
+            cmd.Parameters.AddWithValue("@id", textBox1.Text);
+            cmd.Parameters.AddWithValue("@password", textBox2.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                Form211 admindash = new Form211();
+                admindash.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect id or password ");
+            }
+
         }
 
         private void button2_Click(object sender, EventArgs e)
